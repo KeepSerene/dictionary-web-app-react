50f4c5d8fac94639c3b11d9c9525de7902d4f928
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-01-20 06:26:36 +0530

    handled the case when a pronunciation audio is not available

--- src/components/searchResult/SearchResult.jsx
+++ src/components/searchResult/SearchResult.jsx
@@ -25,43 +25,63 @@ function SearchResult({ result }) {
         <div className="word-audio-wrapper">
           <h1 className="word">{result.word}</h1>
 
-          {/* Audio play/pause button */}
+          {/* Audio play/pause/muted button */}
           <button
             type="button"
             onClick={handleAudioPlay}
-            aria-label={`${isAudioPlaying ? "Pause" : "Play"} pronunciation`}
+            disabled={!audioURL}
+            aria-label={`${
+              audioURL
+                ? isAudioPlaying
+                  ? "Pause pronunciation"
+                  : "Play pronunciation"
+                : "No pronunciation available"
+            }`}
+            style={{ cursor: !audioURL ? "not-allowed" : "" }}
             className="audio-control-btn"
           >
-            {/* Play/pause icon */}
-            {isAudioPlaying ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                aria-hidden="true"
-                className="icon pause"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 5.25v13.5m-7.5-13.5v13.5"
-                />
-              </svg>
+            {/* Play/pause/no-sound icon */}
+            {audioURL ? (
+              isAudioPlaying ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  className="icon pause"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 5.25v13.5m-7.5-13.5v13.5"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="icon play"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              )
             ) : (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
                 aria-hidden="true"
-                className="icon play"
+                className="icon mute"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-                  clipRule="evenodd"
-                />
+                <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM17.78 9.22a.75.75 0 1 0-1.06 1.06L18.44 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 1 0 1.06-1.06L20.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-1.72 1.72-1.72-1.72Z" />
               </svg>
             )}
           </button>
